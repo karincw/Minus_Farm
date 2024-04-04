e6ca15ab94d067b111665b7590ccc2b4d6bc9cd8
Karin <karin.cw07@gmail.com>
2024-04-05 01:33:34 +0900

    SpreadSheetLoader만듬
    https://docs.google.com/spreadsheets/d/1hxz8zOcXDzjCdvcK8zhihAWl85gQUqRsmOrN93Boaqs/edit#gid=0
    여기있는 시트를 유니티에 CardSO로 자동으로 만들어주는 코드

---
+++ Assets/01.Scripts/CW/Core/DragAndDrop.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CW
+{
+    public class DragAndDrop : MonoBehaviour
+    {
+
+        private bool holding = false;
+
+
+    }
+}
