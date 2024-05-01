1122881fadda797c89d2d260c184eb00c2acf81b
plmknbuhv <plmknbuhv@gmail.coom>
2024-05-01 20:53:53 +0900

    메인 터진거

--- Assets/01.Scripts/HS/TopBar/TopBarLeftUi.cs
+++ Assets/01.Scripts/HS/TopBar/TopBarLeftUi.cs
@@ -1,16 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HS
 {
     public class TopBarLeftUi : MonoBehaviour
     {
         [SerializeField] private string[] _weather;
+        [SerializeField] private Sprite[] _weatherIcon;
         [SerializeField] private TextMeshProUGUI _weatherText;
+        [SerializeField] private Image _weatherImage;
 
         public void ChangeWeather()
         {
-            _weatherText.text = _weather[Random.Range(0, 2)];
+            int num = Random.Range(0, 2);
+            _weatherText.text = _weather[num];
+            _weatherImage.sprite = _weatherIcon[num];
         }
 
     }
