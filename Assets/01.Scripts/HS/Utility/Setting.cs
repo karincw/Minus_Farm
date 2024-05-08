6cf5623db0356562b2b28d2d7fff7ec2a407e83b
plmknbuhv <plmknbuhv@gmail.coom>
2024-05-08 20:22:59 +0900

    세팅 마무리

--- Assets/01.Scripts/HS/Utility/Setting.cs
+++ Assets/01.Scripts/HS/Utility/Setting.cs
@@ -1,11 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Setting : MonoBehaviour
 {
-    public void Quit()
+    private Slider _slider;
+    [SerializeField] private AudioMixer _masterMixer;
+
+    private void Awake()
+    {
+        _slider = transform.Find("Slider").GetComponent<Slider>();
+    }
+
+    public void AudioControl()
+    {
+        float sound = _slider.value;
+
+        if (sound == -40f) _masterMixer.SetFloat("BGM", -80);
+        else
+        {
+            _masterMixer.SetFloat("BGM", sound);
+        }
+    }
+
+    public void QuitButton()
     {
         Application.Quit();
     }
+
+    public void TitleButton()
+    {
+        //SceneManager.LoadScene("¹¹½Ã±â ¾À");
+    }
+    
+    
 }
